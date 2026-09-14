840c7ea669879590af537e9aeb73e2fee98e4af5
Aditya Jaiswal <adijaiswal66@gmail.com>
2026-09-14 18:07:38 +0530

    task 21.1 done

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -264,25 +264,23 @@ public class EmployeeService {
 
         for (Employee employee : employees) {
             if (employee.getRating() == 5) {
-                List<Employee> employeeList = employeesByCategory.get("Excellent");
                 employeesByCategory.computeIfAbsent(("Excellent"), key -> new ArrayList<>())
                         .add(employee);
             } else if (employee.getRating() == 4) {
-                List<Employee> employeeList = employeesByCategory.get("Good");
                 employeesByCategory.computeIfAbsent(("Good"), key -> new ArrayList<>())
                         .add(employee);
             } else if (employee.getRating() == 3) {
-                List<Employee> employeeList = employeesByCategory.get("Average");
                 employeesByCategory.computeIfAbsent(("Average"), key -> new ArrayList<>())
                         .add(employee);
             } else if (employee.getRating() == 2) {
-                List<Employee> employeeList = employeesByCategory.get("Below Average");
                 employeesByCategory.computeIfAbsent(("Below Average"), key -> new ArrayList<>())
                         .add(employee);
             } else if (employee.getRating() == 1) {
-                List<Employee> employeeList = employeesByCategory.get("Poor");
                 employeesByCategory.computeIfAbsent(("Poor"), key -> new ArrayList<>())
                         .add(employee);
+            }
+            if ((employee.getRating() < 1) || (employee.getRating() > 5)) {
+                throw new IllegalArgumentException("Rating should be between 1-5");
             }
         }
 
